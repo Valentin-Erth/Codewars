2fcf43557b8e10a535e77063ec296e2843a446c7
Atom <kozlov0020@gmail.com>
2022-12-21 16:45:25 +0500

    answer task

---
+++ Thinkful - Logic _Drills_Traffic light.js
@@ -0,0 +1,20 @@
+function updateLight(current) {
+    switch (current) {
+        case "green":
+            return "yellow";
+        case "yellow":
+            return "red";
+        case "red":
+            return "green";
+    }
+}
+
+function updateLight(current) {
+    return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+}
+
+const updateLight = current => ({
+    green: 'yellow',
+    yellow: 'red',
+    red: 'green',
+})[current]
