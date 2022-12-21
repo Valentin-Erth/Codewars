71032bbb9d30a3c74832cbfc590d8ef44aae7276
Atom <kozlov0020@gmail.com>
2022-12-21 18:24:02 +0500

    answer task

--- Part1/SwitchItUp.js
+++ Part1/SwitchItUp.js
@@ -19,7 +19,21 @@ function switchItUp(number) {
             return "Four";
         case 5:
             return "Five";
+        case 6:
+            return "Six";
+        case 7:
+            return "Seven";
+        case 8:
+            return "Eight";
+        case 9:
+            return "Nine";
+
         default:
             return "unknown";
     }
 }
+
+// function switchItUp(number){
+//     let array = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+//     return array[number];
+//   }
