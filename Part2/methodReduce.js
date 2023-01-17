d26d27b9f064fc861302a1e47206b45fa6aeb2bf
Atom <kozlov0020@gmail.com>
2023-01-17 16:00:15 +0500

    answer task method Math

--- Part2/methodReduce.js
+++ Part2/methodReduce.js
@@ -45,3 +45,16 @@ function tailAndHead(arr) {
         .reduce((x, y, i, r) => i == 0 ? [] : x.concat(Number(r[i - 1][r[i - 1].length - 1]) + Number(y[0])), [])
         .reduce((a, b) => a * b);
 }
+let myNestedArray = [
+    // Only change code below this line
+    ['unshift', [false, 1, 2, 3, 'complex', 'deep'], 'nested'],
+    [
+        ['loop', ['shift', 'deeper', [6, 7, 'deepest'], 1000, ]
+            'method'
+        ],
+        ['concat', false, true, 'spread', 'array'],
+        ['mutate', 1327.98, 'splice', 'slice', 'push'],
+        ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+    ]
+    // Only change code above this line
+];
