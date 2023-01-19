564a6d27eb76bcf05acc57d20720585bc02216b7
Atom <kozlov0020@gmail.com>
2023-01-19 18:38:35 +0500

    answer task typeof el not string

---
+++ Part3/filterString.js
@@ -0,0 +1,11 @@
+// В этом ката вы создадите функцию, которая берет список неотрицательных целых чисел и строк и возвращает новый список с отфильтрованными строками.
+
+// Пример
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    let newArr = l.filter((el) => typeof(el) != 'string');
+    return newArr;
+}
