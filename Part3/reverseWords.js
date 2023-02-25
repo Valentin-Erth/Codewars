93e4a15075487dfb408cf1b260f06b9f55257c01
Kozlov Valentin <kozlov0020@gmail.com>
2023-02-25 14:14:58 +0500

    answer task revers words in str words

---
+++ Part3/reverseWords.js
@@ -0,0 +1,9 @@
+// Завершите функцию, которая принимает строковый параметр и переворачивает каждое слово в строке. Все пробелы в строке должны быть сохранены.
+//
+//     Примеры
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+    return str.split(' ').map(el=>el.split("").reverse().join("")).join(" ")
+}
