3c31dd687c71f546abc1e7e47a9c76444f0cce6a
Kozlov Valentin <kozlov0020@gmail.com>
2023-03-29 14:28:16 +0500

    make task cw shortsword

---
+++ Part3/ShortestWord.js
@@ -0,0 +1,8 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+//
+//     String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    let arr=s.split(" ").map(w=>w.length).sort((a,b)=>a-b)
+    return arr[0]
+}
