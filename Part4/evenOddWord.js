39857ee376e325190a44a229852909b7d28e4d99
Kozlov Valentin <kozlov0020@gmail.com>
2023-08-08 16:27:20 +0500

    answer CW make even and odd in word

---
+++ Part4/evenOddWord.js
@@ -0,0 +1,13 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+//
+//     For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+//
+//     The input will be a lowercase string with no spaces.
+//
+//     Good luck!
+
+function capitalize(s) {
+    const evenWord = s.split('').map((el,i)=> i % 2 === 0 || i === 0 ? el.toUpperCase() : el).join('')
+    const oddWord=s.split('').map((el,i)=>i%2!==0? el.toUpperCase(): el).join('')
+    return [evenWord,oddWord]
+}
